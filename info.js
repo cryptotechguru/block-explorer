ade316bf75f4b1c92a43d664d9cff183fe786d82
KvagaJack <kvagajack@gmail.com>
2018-12-01 00:03:33 -0500

    debug version u/p

--- info.js
+++ info.js
@@ -9,11 +9,15 @@ module.exports = function (app) {
   version:  ${packageJson.version},
   env: {
     NODE_ENV: ${process.env.NODE_ENV},
-    EQUIBIT_CORE_URL: ${process.env.EQUIBIT_CORE_URL}
+    EQUIBIT_CORE_URL: ${process.env.EQUIBIT_CORE_URL},
+    EQUIBIT_CORE_USERNAME: ${process.env.EQUIBIT_CORE_USERNAME && process.env.EQUIBIT_CORE_USERNAME.substring(0, 3)}...,
+    EQUIBIT_CORE_PASSWORD: ${process.env.EQUIBIT_CORE_PASSWORD && process.env.EQUIBIT_CORE_PASSWORD.substring(0, 3)}...
   },
   wallet: {
     host: ${walletConfig.host},
-    port: ${walletConfig.port}
+    port: ${walletConfig.port},
+    user: ${walletConfig.user && walletConfig.user.substring(0, 3)}...,
+    pass: ${walletConfig.pass && walletConfig.pass.substring(0, 3)}...
   }
 }`
     );
